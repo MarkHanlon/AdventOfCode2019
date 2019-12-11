4026e3f9a5f3184946c432f65681e7de92a7e740
Mark Hanlon <mark@markhanlon.com>
2019-12-11 22:39:32 +0000

    Completed part 2

--- Day7/Part2/Program.cs
+++ Day7/Part2/Program.cs
@@ -22,18 +22,37 @@ namespace Day7
 
             List<List<int>> allPerms = FindPermutations(new List<int>{5,6,7,8,9});
             int highestOutput = 0;
+            
             foreach (List<int> thisPerm in allPerms)
-            {                
-                int input = 0;
-                for (int i = 0; i < thisPerm.Count(); i++)
+            //List<int> thisPerm = new List<int> {9,8,7,6,5};
+            {          
+                // Try a new permutation - reset the computers!                      
+                var computers = new IntcodeComputer[] {new IntcodeComputer(), new IntcodeComputer(), new IntcodeComputer(), new IntcodeComputer(), new IntcodeComputer()};
+                for (int i = 0; i <= 4; i++)
                 {
-                    var computer = new IntcodeComputer();
-                    computer.Load(programCode);
-                    var outputs = computer.Run(new int[] {thisPerm[i],input});
-                    input = outputs[0];
+                    computers[i].Load(programCode);
+                    computers[i].AddInput(thisPerm[i]);
                 }
+
+                int input = 0;
+                bool running = true;
+                while (running)
+                {
+                    for (int i = 0; i < thisPerm.Count(); i++)
+                    {                                                                                
+                        computers[i].AddInput(input);
+                        int output = computers[i].Run();
+                        if (computers[i].IsFinished)
+                            running = false;
+
+                        input = output;
+                    }
+                }
+
+                // All programs finished, is the answer the best yet?
                 if (input > highestOutput)
                     highestOutput = input;
+                
             }
             timer.Stop();
             Console.WriteLine($"Done with output: {highestOutput}, execution in {timer.ElapsedMilliseconds}ms");
@@ -56,12 +75,25 @@ namespace Day7
     class IntcodeComputer
     {
         private string _program;
-        int[] ram;
+        int[] _ram;
+        int _pc;
+        bool _isFinished;
+
+        Queue<int> _inputs;
+        int _output;
 
         public void Load(string program)
         {
             _program = program;
+            _inputs = new Queue<int>();
+            _output = -1;
+          
             Reset();
+        }
+
+        public void AddInput(int input)
+        {
+            _inputs.Enqueue(input);
         }
 
         public void Reset()
@@ -74,24 +106,23 @@ namespace Day7
                 ramList.Add(Int32.Parse(command));
             }
             
-            ram = ramList.ToArray();
+            _ram = ramList.ToArray();
+            _pc = 0;
+            IsFinished = false;
         }
 
-        public int[] Run(int[] inputs)
+        public bool IsFinished { get; private set; }
+        public int Run()
         {
             try
-            {
-                List<int> outputs = new List<int>();
-                int nextInput = 0;
-
-                int pc = 0;
-                while (ram[pc] != 99)
+            {                                
+                while (_ram[_pc] != 99)
                 {
                     // Extract the op-code
-                    int opCode = ram[pc] % 100;
+                    int opCode = _ram[_pc] % 100;
                     int[] pMode = new int[10];   // 0 = position mode, 1 = immediate mode
                     int i = 0;
-                    foreach (char c in ram[pc].ToString().Reverse().Skip(2))
+                    foreach (char c in _ram[_pc].ToString().Reverse().Skip(2))
                     {
                         pMode[i++] = Int16.Parse(c.ToString());
                     }
@@ -99,48 +130,57 @@ namespace Day7
                     switch (opCode)
                     {
                         case 1: // Add [p1] + [p2] -> [p3]
-                            ram[ram[pc+3]] = DecodeParam(pMode[0], pc+1) + DecodeParam(pMode[1], pc+2);
-                            pc+=4;
+                            _ram[_ram[_pc+3]] = DecodeParam(pMode[0], _pc+1) + DecodeParam(pMode[1], _pc+2);
+                            _pc+=4;
                             break;
                         case 2: // Multiply [p1] * [p2] -> [p3]
-                            ram[ram[pc+3]] = DecodeParam(pMode[0], pc+1) * DecodeParam(pMode[1], pc+2);
-                            pc+=4;
+                            _ram[_ram[_pc+3]] = DecodeParam(pMode[0], _pc+1) * DecodeParam(pMode[1], _pc+2);
+                            _pc+=4;
                             break;
                         case 3: // Input -> [p1]
-                            Console.WriteLine($"Input required: ({inputs[nextInput]})");                            
-                            ram[ram[pc+1]] = inputs[nextInput++];
-                            pc+=2;
+                            if (_inputs.Count > 0)
+                            {
+                                Console.WriteLine($"Input required: ({_inputs.Peek()})");                            
+                                _ram[_ram[_pc+1]] = _inputs.Dequeue();
+                                _pc+=2;
+                            }
+                            else
+                            {
+                                // Need to return to await a next input, via 'Continue'
+                                Console.WriteLine("Input required: <empty - awaiting input>");
+                                return _output;
+                            }
                             break;
                         case 4: // Output <- [p1]
-                            outputs.Add(DecodeParam(pMode[0], pc+1));
-                            Console.WriteLine($"Output: {outputs.Last()}");
-                            pc+=2;
+                            _output = DecodeParam(pMode[0], _pc+1);
+                            Console.WriteLine($"Output: {_output}");
+                            _pc+=2;
                             break;
                         case 5: // Jump if true (if [p1] != 0 [p2] -> pc)
-                            if (DecodeParam(pMode[0], pc+1) != 0)
-                                pc = DecodeParam(pMode[1], pc+2);
+                            if (DecodeParam(pMode[0], _pc+1) != 0)
+                                _pc = DecodeParam(pMode[1], _pc+2);
                             else
-                                pc+=3;
+                                _pc+=3;
                             break;
                         case 6: // Jump if false (if [p1] == 0 [p2] -> pc)
-                            if (DecodeParam(pMode[0], pc+1) == 0)
-                                pc = DecodeParam(pMode[1], pc+2);
+                            if (DecodeParam(pMode[0], _pc+1) == 0)
+                                _pc = DecodeParam(pMode[1], _pc+2);
                             else
-                                pc+=3;
+                                _pc+=3;
                             break;
                         case 7: //less than (if [p1] < [p2] 1 => [p3], else 0 => [p3])
-                            if (DecodeParam(pMode[0], pc+1) < DecodeParam(pMode[1], pc+2))
-                                ram[ram[pc+3]] = 1;
+                            if (DecodeParam(pMode[0], _pc+1) < DecodeParam(pMode[1], _pc+2))
+                                _ram[_ram[_pc+3]] = 1;
                             else
-                                ram[ram[pc+3]] = 0;
-                            pc+=4;
+                                _ram[_ram[_pc+3]] = 0;
+                            _pc+=4;
                             break;
                         case 8: //equals (if [p1] == [p2] 1 => [p3], else 0 => [p3])
-                            if (DecodeParam(pMode[0], pc+1) == DecodeParam(pMode[1], pc+2))
-                                ram[ram[pc+3]] = 1;
+                            if (DecodeParam(pMode[0], _pc+1) == DecodeParam(pMode[1], _pc+2))
+                                _ram[_ram[_pc+3]] = 1;
                             else
-                                ram[ram[pc+3]] = 0;
-                            pc+=4;
+                                _ram[_ram[_pc+3]] = 0;
+                            _pc+=4;
                             break;
                         default:
                             throw new Exception("Unknown command - program terminated");
@@ -149,19 +189,20 @@ namespace Day7
                 }
 
                 // Done
-                Console.WriteLine($"Complete: Outputs returned: {string.Join(',', outputs.ToArray())}");
-                return outputs.ToArray();
+                Console.WriteLine($"Complete: Outputs returned: {string.Join(',', _output)}");
+                IsFinished = true;
+                return _output;
                 
             }
             catch (Exception ex)
             {
                 Console.WriteLine($"Exception caught: {ex.Message}");
-                return null;
+                return -99;
             }
         }
         private int DecodeParam(int paramMode, int paramAddr)
         {
-            return (paramMode == 0 ? ram[ram[paramAddr]] : ram[paramAddr]);
+            return (paramMode == 0 ? _ram[_ram[paramAddr]] : _ram[paramAddr]);
         }
 
 
